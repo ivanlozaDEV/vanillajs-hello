bd29acf9a59148c5c4bdc651b40d4f95b6769ce9
ivanlozaDEV <159062304+ivanlozaDEV@users.noreply.github.com>
2024-02-29 03:42:28 +0000

    Pending changes exported from your codespace

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let excuseEl = document.getElementById("excuse-el");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+var whoRandom = who[Math.floor(Math.random() * who.length)];
+
+var actionRandom = action[Math.floor(Math.random() * action.length)];
+
+var whatRandom = what[Math.floor(Math.random() * what.length)];
+
+var whenRandom = when[Math.floor(Math.random() * when.length)];
+
+function excuseChanger() {
+  let excuse =
+    whoRandom + " " + actionRandom + " " + whatRandom + " " + whenRandom;
+
+  excuseEl.textContent = excuse;
+}
+excuseChanger();
